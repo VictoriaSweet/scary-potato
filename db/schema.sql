77d4eb1225e1badbf584834fad97c3aab0f025e1
dmod129 <derek.modugno@gmail.com>
2023-11-09 21:32:59 -0600

    created database

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+{
+    "game": {
+        "players": [
+            {
+                "name": "Player",
+                "hand": [],
+                "books": []
+            },
+            {
+                "name": "Computer",
+                "hand": [],
+                "books": []
+            }
+        ],
+        "deck": [],
+        "books_pile": [],
+        "current_player": 0
+    }
+}
