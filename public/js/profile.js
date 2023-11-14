073de2967872b7a06df6aec188402086e6c888ab
Matthew <Deweasematthew@gmail.com>
2023-11-13 21:42:14 -0600

    added semantic HTML

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,19 @@
 document.querySelector("form").addEventListener("submit", function (event) {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  let formValid = true;
 
   if (username === "") {
     alert("Please enter your username");
-    event.preventDefault(); // Prevent form submission
+    formValid = false;
   }
 
   if (password === "") {
     alert("Please enter your password");
+    formValid = false;
+  }
+
+  if (!formValid) {
     event.preventDefault(); // Prevent form submission
   }
 });
