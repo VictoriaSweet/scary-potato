95ca75f783866370b152416d2d29bfd85581ea78
dmod129 <derek.modugno@gmail.com>
2023-11-16 19:22:41 -0600

    started plantRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const plantRoutes = require('./plantRoutes');
+// const plantRoutes = require('./plantRoutes');
 
 router.use('/users', userRoutes);
-router.use('/plant', plantRoutes);
+// router.use('/plant', plantRoutes);
 
 module.exports = router;
---
+++ controllers/api/plantRoutes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { Plant } = require('../../models');
+
+GET https://perenual.com/api/species-list?key=[sk-l9HY654e6939256ac2895]
