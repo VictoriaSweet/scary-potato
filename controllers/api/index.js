928944c6725578f4cf92798a511c26fdba07c8f6
dmod129 <derek.modugno@gmail.com>
2023-11-16 19:05:20 -0600

    made userRoutes and index routes

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const plantRoutes = require('./plantRoutes');
+
+router.use('/users', userRoutes);
+router.use('/plant', plantRoutes);
+
+module.exports = router;
