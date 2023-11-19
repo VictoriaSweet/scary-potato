ce4665ba77a34e8e7314972c62f7066d9b6af895
dmod129 <derek.modugno@gmail.com>
2023-11-19 17:06:44 -0600

    added seeds folder with seed.js, plantData.json, and userData.json

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO plants (plant_name, edible, poisonous, cycle, watering, sunlight, indoor)
-VALUES 
-    ("European Silver Fir", false, false, "Perrennial", "Frequent","Part shade", false);
---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Plant } = require('../models');
+
+const userData = require('./userData.json');
+const plantData = require('./plantData.json')
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const plant of plantData) {
+    await Plant.create({
+      ...plant,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
