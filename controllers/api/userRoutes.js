f70a002a6488bdc9c6fa5024042d0954f69f5709
VictoriaSweet <sweet.victoria1218@gmail.com>
2023-11-21 17:14:02 -0600

    uncommented out code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -44,7 +44,7 @@ router.post('/login', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      // res.json({ user: userData, message: 'You are now logged in!' });
+      res.json({ user: userData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
